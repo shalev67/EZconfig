2cedba67fbfe0ca448ccf7134b51e4e9329a6d16
Yuval Shalev <shalev67@gmail.com>
2018-09-13 19:49:37 +0300

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ezcfg',
-    version='0.1.2',
+    version='0.1.3',
     description="Simple python configuration tool",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
